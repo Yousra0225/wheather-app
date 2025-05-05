404cb311570faa56661708cdaabdb7b0d2728d19
Yousra0225 <chbibyousra02@gmail.com>
2025-05-05 15:10:17 +0200

    feat(graph-zone): add graph visibility toggle and state management for data type

---
+++ src/components/displayGraphZone.component.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import DisplayGraph from './displayGraph.component.jsx';
+import meteoDataByCity from '../data/meteo-data.js';
+
+const DisplayGraphZone = ({ selectedCity, defaultDataType, favoriteCity }) => {
+  const [graphVisible, setGraphVisible] = useState(false);
+  const [dataType, setDataType] = useState(defaultDataType);
+
+  const [changeDataClicked, setChangeDataClicked] = useState(false);
+
+  const toggleGraph = () => {
+    setGraphVisible(!graphVisible);
+  }
+}
+
+export default DisplayGraphZone;
