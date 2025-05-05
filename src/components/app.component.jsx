efa513fd578dab2951f3ada074ba3bdd60d84d2b
Yousra0225 <chbibyousra02@gmail.com>
2025-05-05 15:26:39 +0200

    feat(app): initialize App component with state for selected and favorite cities

---
+++ src/components/app.component.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import DisplayDataCity from './displayDataCity.component.jsx';
+import meteoDataByCity from '../data/meteo-data.js';
+import DisplayGraph from './displayGraph.component.jsx';
+import SelectCity from './selectCity.component.jsx';
+import DisplayGraphZone from './displayGraphZone.component.jsx';
+import ComparisonData from './comparisonData.component.jsx';
+
+
+const App = () => {
+
+  const defaultCity = 'Lille';
+  const defaultDataType = 'pluviometrie';
+  const [selectedCity, setSelectedCity] = useState(defaultCity);
+  const [favoriteCity, setFavoriteCity] = useState(defaultCity);
+  const [cityData, setCityData] = useState(null);
+  const [favoriteCityData, setFavoriteCityData] = useState(null);
+
+  const selectedCityData = meteoDataByCity.find(item => item.city === selectedCity);
+  const selectedFavoriteCityData = meteoDataByCity.find(item => item.city === favoriteCity);
+  
+};
+
+export default App;
